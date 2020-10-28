10db1443eaf1b3fcc83a8ab24ca0ad636079e493
James Myatt <james@jamesmyatt.co.uk>
2020-10-28 22:25:38 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,5 @@
-install.packages("rstan", repos = "https://cloud.r-project.org/", dependencies = TRUE)
+install.packages(
+  c("tidyverse", "rstan", "stringr", "lubridate", "gridExtra", "pbapply", "parallel", "boot", "lqmm", "ggrepel"),
+  repos = "https://cloud.r-project.org/",
+  dependencies = TRUE
+)
